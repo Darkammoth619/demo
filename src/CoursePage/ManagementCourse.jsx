8ec8d683cfaacfebf884acf24627883fed732e09
Dario <dario_cabrera1994@hotmail.com>
2020-07-10 14:53:07 -0600

    layout fixed

--- src/CoursePage/ManagementCourse.jsx
+++ src/CoursePage/ManagementCourse.jsx
@@ -1,12 +1,10 @@
 import React from 'react';
-import Container from '@material-ui/core/Container';
 import ManagementCourseLayout from './ManagementCourseLayout';
-import Banner from '../Banner';
 
 const ManagementCourse=()=>{
 return(
         <div>
-            <Banner/>
+            {/* <Banner/> */}
             <ManagementCourseLayout/>
         </div>
     );
